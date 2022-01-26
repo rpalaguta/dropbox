7492d84132ffcbbb56bb829bbc7201ae95961776
Jevgenij Cchovrebov <cchovrebov@gmail.com>
2022-01-26 20:39:51 +0200

    Remove, capacity

--- src/script.js
+++ src/script.js
@@ -17,32 +17,36 @@
 //   }
 // }
 // render();
-const images = [];
+let images = [];
 var upload = document.getElementById("upload");
 
-upload.onchange = function(e) { 
-  const images2 = [];
+let selectedItems = [];
+
+upload.onchange = function (e) {
   for (let i = 0; i < document.getElementById("upload")?.files.length; i++) {
     const element = document.getElementById("upload")?.files[i];
-    images2.push({
+    images.push({
       name: element.name,
       size: element.size,
       date: element.lastModifiedDate,
       path: URL.createObjectURL(element),
     });
   }
-  render(images2);
+
+  render(images);
 };
 
 const render = (arr) => {
-  arr?.forEach((element) => {
+  document.getElementById("content").innerHTML = '';
+  arr?.forEach((element, index) => {
     const imageItem = document.createElement("div");
+
     const image = document.createElement("div");
     const h2 = document.createElement("h2");
     const h3 = document.createElement("h3");
     const h4 = document.createElement("h4");
     h2.textContent = element.name;
-    h3.textContent = element.size;
+    h3.textContent = `${convertBytes(element.size)} MB`;
     h4.textContent = element.date;
     imageItem.className = "uploaded";
     image.className = "uploaded-img";
@@ -51,12 +55,44 @@ const render = (arr) => {
     imageItem.appendChild(h2);
     imageItem.appendChild(h3);
     imageItem.appendChild(h4);
+    imageItem.setAttribute('id', index);
+    const selectedDiv = document.createElement("div");
+    selectedDiv.setAttribute('class', 'selected');
+    imageItem.appendChild(selectedDiv);
     document.getElementById("content").appendChild(imageItem);
+
+    imageItem.addEventListener('click', function () {
+      this.classList.toggle('active');
+      if (selectedItems.includes(this.id)) {
+        const indexOf = selectedItems.indexOf(this.id);
+        selectedItems.splice(indexOf, 1);
+      } else {
+        selectedItems.push(this.id);
+      }
+    });
   });
+  getCapacity(arr);
 };
 
 render(images);
 
 document.getElementById('delete').addEventListener('click', function () {
-  document.getElementById('content').removeChild(document.getElementById('content').childNodes[0]);
-})
+  images = images.filter((item, index) => !selectedItems.includes(index.toString()));
+  console.log(selectedItems, images);
+  selectedItems = [];
+  render(images);
+});
+
+function convertBytes(bytes) {
+  return (bytes / 1048576).toFixed(2);
+}
+
+function getCapacity(arr) {
+  let capacityBytes = 0;
+  for (let item of arr) {
+    capacityBytes += item.size;
+  }
+  const capacityMb = convertBytes(capacityBytes);
+  document.getElementById('capacityVal').textContent = `${capacityMb} MB / 100 MB`;
+  document.getElementById('capacity').value = capacityMb;
+}
