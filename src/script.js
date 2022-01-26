95acd0ceea5d8a3cef89780a9b2dda97f4f2d3fa
Raimondas Palaguta <r.palaguta@gmail.com>
2022-01-26 20:48:33 +0200

    asd

--- src/script.js
+++ src/script.js
@@ -18,10 +18,11 @@
 // }
 // render();
 const images = [];
+let sizeSum = 0;
 var upload = document.getElementById("upload");
 
 upload.onchange = function(e) { 
-  const images2 = [];
+  const images2 = []
   for (let i = 0; i < document.getElementById("upload")?.files.length; i++) {
     const element = document.getElementById("upload")?.files[i];
     images2.push({
@@ -30,19 +31,23 @@ upload.onchange = function(e) {
       date: element.lastModifiedDate,
       path: URL.createObjectURL(element),
     });
+    sizeSum += element.size;
   }
+  
+  // console.log(convertBytes(sizeSum));
   render(images2);
 };
-
+let index = 0;
 const render = (arr) => {
   arr?.forEach((element) => {
+    index++;
     const imageItem = document.createElement("div");
     const image = document.createElement("div");
     const h2 = document.createElement("h2");
     const h3 = document.createElement("h3");
     const h4 = document.createElement("h4");
     h2.textContent = element.name;
-    h3.textContent = element.size;
+    h3.textContent = convertBytes(element.size);
     h4.textContent = element.date;
     imageItem.className = "uploaded";
     image.className = "uploaded-img";
@@ -51,6 +56,10 @@ const render = (arr) => {
     imageItem.appendChild(h2);
     imageItem.appendChild(h3);
     imageItem.appendChild(h4);
+    imageItem.setAttribute('id', index);
+    imageItem.addEventListener('click', {
+      imageItem.setAttribute('id', 'active');
+    })
     document.getElementById("content").appendChild(imageItem);
   });
 };
@@ -60,3 +69,11 @@ render(images);
 document.getElementById('delete').addEventListener('click', function () {
   document.getElementById('content').removeChild(document.getElementById('content').childNodes[0]);
 })
+
+function convertBytes(bytes) {
+return (bytes / 1048576).toFixed(2);
+}
+
+// document.getElementById('storage').innerText(${})
+
+// console.log();
