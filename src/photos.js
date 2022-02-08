75231b010a0bdeeb62d3096d5153e34ee6b8bbe1
Raimondas Palaguta <r.palaguta@gmail.com>
2022-02-08 21:58:47 +0200

    update

---
+++ src/photos.js
@@ -0,0 +1,132 @@
+import { getPhotos, getPhoto, patchPhoto, deletePhoto, postPhoto } from '/photos_helper.js';
+
+const photos = await getPhotos(20);
+console.log({ photos });
+
+let images = [];
+var upload = document.getElementById('upload');
+let delList = [];
+
+// upload.onchange = async function (e) {
+//   for (let i = 0; i < upload?.files?.length; i++) {
+//     const element = upload?.files[i];
+//     const data = {
+//       title: element.name,
+//       size: element.size,
+//       date: moment().format('MMMM Do YYYY, h:mm:ss'),
+//       url: URL.createObjectURL(element),
+//     };
+//     let createdelement = await postPhoto(data);
+//     console.log(createdelement);
+//   }
+//   // render(images);
+// };
+
+document.getElementById('upload').addEventListener('change', async function () {
+  for (let i = 0; i < this.files.length; i++) {
+    const file = this.files[i];
+    const formData = new FormData();
+    formData.append('file', file, file.name);
+    const createdItem = await postPhoto(formData);
+    photos.unshift(createdItem);
+  }
+
+  render(photos);
+});
+
+const render = (arr) => {
+  let sizeSum = 0;
+  document.getElementById('content').innerHTML = '';
+  arr?.forEach((element, index) => {
+    sizeSum += element.size;
+    const imageItem = document.createElement('div');
+    const image = document.createElement('div');
+    const checked = document.createElement('div');
+    const mark = document.createElement('img');
+    const h2 = document.createElement('h2');
+    const h3 = document.createElement('h3');
+    const h4 = document.createElement('h4');
+    h2.textContent = element.title;
+    h3.textContent = convertBytes(element.size) + ' MB';
+    h4.textContent = element.date;
+    mark.setAttribute('src', 'img/mark.png');
+    imageItem.className = 'uploaded';
+    image.className = 'uploaded-img';
+    checked.className = 'checked';
+    mark.className = 'check-mark';
+    image.setAttribute('style', `background-image: url(${element.url})`);
+    imageItem.appendChild(image);
+    image.appendChild(checked);
+    checked.appendChild(mark);
+    imageItem.appendChild(h2);
+    imageItem.appendChild(h3);
+    imageItem.appendChild(h4);
+    imageItem.setAttribute('id', element.id);
+    document.getElementById('content').appendChild(imageItem);
+
+    imageItem.addEventListener('click', function () {
+      checked.classList.toggle('active');
+      if (delList.includes(this.id)) {
+        const indexOf = delList.indexOf(checked.id);
+        delList.splice(indexOf, 1);
+      } else {
+        delList.push(this.id);
+      }
+      console.log(delList);
+    });
+    imageItem.addEventListener('dblclick', function () {
+      const sphoto = getPhoto(this.id);
+      document.getElementById('preview').style.display = 'flex';
+      document.getElementById('previewContent').style.backgroundImage = `url(${element.url})`;
+    });
+  });
+  // console.log(images);
+  document.getElementById('capacity').textContent = `${convertBytes(sizeSum)} / 1 GB`;
+  document.getElementById('capValue').value = convertBytes(sizeSum);
+  console.log(convertBytes(sizeSum));
+};
+
+// render(images);
+render(photos);
+
+document.getElementById('delete').addEventListener('click', async function () {
+  delList?.forEach(async (id) => {
+    await deletePhoto(id);
+    delList = [];
+  });
+  // setTimeout(async () => {
+  //   const newPhotos = await getPhotos(100);
+  //   photos = newPhotos
+  //   render(photos);
+
+  // }) 1000;
+});
+
+function convertBytes(bytes) {
+  return (bytes / 1048576).toFixed(2);
+}
+
+document.getElementById('sort-name').addEventListener('click', function () {
+  images.sort(function (x, y) {
+    let a = x.name.toUpperCase(),
+      b = y.name.toUpperCase();
+    return a == b ? 0 : a > b ? 1 : -1;
+  });
+  render(images);
+});
+
+document.getElementById('sort-size').addEventListener('click', function () {
+  images.sort(function (x, y) {
+    return x.size - y.size;
+  });
+  render(images);
+});
+
+document.getElementById('sort-modified').addEventListener('click', function () {
+  images.sort(function (x, y) {
+    let a = new Date(x.Date),
+      b = new Date(y.Date);
+    return a - b;
+  });
+  render(images);
+});
--- src/users.js
+++ src/users.js
@@ -1,33 +1,18 @@
-// import { getPosts, deletePost, getPost, createPost, patchPost } from './posts_helpers.js';
+import { onSearch } from './event_helpers.js';
+import { getUsers, getUser, createUser, patchUser, deleteUser } from './users_helpers.js';
 
-// let table = document.createElement('table');
-// let thead = document.createElement('thead');
-// let tbody = document.createElement('tbody');
+let data = [];
 
-// table.appendChild(thead);
-// table.appendChild(tbody);
+document
+  .getElementById('search')
+  .addEventListener('input', (e) => onSearch(e, data, ['name', 'username', 'email'], renderTable));
 
-// document.getElementById('content').appendChild(table);
-// let users = getPosts();
-// console.log(users);
+getUsers().then((res) => {
+  data = res;
+  renderTable(res);
+});
 
-// const API_URL = 'https://jsonplaceholder.typicode.com';
-// function getPosts(id) {
-//   return fetch(`${API_URL}/users/${id}`).then((data) => data.json());
-// }
-
-// getPosts(1)
-//   .then((data) => console.log(data))
-//   .catch((err) => console.log(err));
-
-async function loadData() {
-  const response = await fetch('https://jsonplaceholder.typicode.com/users');
-  const data = await response.json();
-  renderTable(data);
-  console.log(data);
-}
-loadData();
-
+// Render table
 function renderTable(arr) {
   document.getElementById('content').innerHTML = '';
   const table = document.createElement('table');
@@ -35,23 +20,52 @@ function renderTable(arr) {
   const tbody = document.createElement('tbody');
   let row_1 = document.createElement('tr');
   let heading_1 = document.createElement('th');
-  heading_1.innerHTML = 'ID';
+  heading_1.innerHTML = 'Actions';
   let heading_2 = document.createElement('th');
-  heading_2.innerHTML = 'Name';
+  heading_2.innerHTML = 'ID';
   let heading_3 = document.createElement('th');
-  heading_3.innerHTML = 'User name';
+  heading_3.innerHTML = 'Name';
   let heading_4 = document.createElement('th');
-  heading_4.innerHTML = 'Email';
+  heading_4.innerHTML = 'User name';
+  let heading_5 = document.createElement('th');
+  heading_5.innerHTML = 'Email';
   row_1.appendChild(heading_1);
   row_1.appendChild(heading_2);
   row_1.appendChild(heading_3);
   row_1.appendChild(heading_4);
+  row_1.appendChild(heading_5);
   thead.appendChild(row_1);
   table.appendChild(thead);
   table.appendChild(tbody);
   arr.forEach((element, id) => {
     const row = document.createElement('tr');
     table.appendChild(row);
+    let delUser = document.createElement('i');
+    let viewUser = document.createElement('i');
+    let editUser = document.createElement('i');
+    delUser.setAttribute('class', 'fas fa-ban', 'id', `id`);
+    delUser.setAttribute('id', `ban${element.id}`);
+    viewUser.setAttribute('class', 'far fa-eye');
+    viewUser.setAttribute('id', `view${element.id}`);
+    editUser.setAttribute('class', 'fas fa-user-edit');
+    editUser.setAttribute('id', `edit${element.id}`);
+    delUser.addEventListener('click', function () {
+      console.log('ban', element.id);
+    });
+    viewUser.addEventListener('click', function () {
+      document.getElementById('popup').style.display = 'flex';
+      userEvent('view', element.id);
+      // console.log(document.getElementById('pop-up'));
+    });
+    editUser.addEventListener('click', function () {
+      document.getElementById('popup').style.display = 'flex';
+      userEvent('edit', element.id);
+    });
+    let actions = document.createElement('td');
+    actions.appendChild(delUser);
+    actions.appendChild(viewUser);
+    actions.appendChild(editUser);
+    row.appendChild(actions);
     let number = document.createElement('td');
     number.innerHTML = `${element.id}`;
     row.appendChild(number);
@@ -64,6 +78,199 @@ function renderTable(arr) {
     let email = document.createElement('td');
     email.innerHTML = `${element.email}`;
     row.appendChild(email);
+    document.getElementById('content').appendChild(table);
   });
-  document.getElementById('content').appendChild(table);
+}
+
+document.getElementById('closePopup').addEventListener('click', function () {
+  document.getElementById('popup').style.display = 'none';
+  document.getElementById('userData').innerHTML = '';
+});
+
+function userEvent(type, id) {
+  console.log(type, id);
+  switch (type) {
+    case 'view':
+      getUser(id).then((res) => renderUserData(res));
+      break;
+    case 'edit':
+      getUser(id).then((res) => editUserData(res));
+      break;
+    case 'delete':
+      break;
+    default:
+      break;
+  }
+}
+
+// View User data
+function renderUserData({
+  id,
+  name,
+  username,
+  email,
+  phone,
+  website,
+  address: { street, suite, city, zipcode },
+  company: { name: companyName, catchPhrase, bs },
+}) {
+  document.getElementById('userData').innerHTML = `
+    <div class="user-data">
+      <div>
+        <h6>PERSONAL DATA</h6>
+        <div>ID: ${id}</div>
+        <div>NAME: ${name}</div>
+        <div>USERNAME: ${username}</div>
+        <div>EMAIL: ${email}</div>
+        <div>PHONE: ${phone}</div>
+        <div>WEBSITE: ${website}</div>
+      </div>
+      <div>
+        <h6>ADDRESS</h6>
+        <div>STREET: ${street}</div>
+        <div>SUITE: ${suite}</div>
+        <div>CITY: ${city}</div>
+        <div>ZIPCODE: ${zipcode}</div>
+      </div>
+      <div>
+        <h6>COMPANY</h6>
+        <div>NAME: ${companyName}</div>
+        <div>CATCH PHRASE: ${catchPhrase}</div>
+        <div>BS: ${bs}</div>
+      </div>
+    </div>
+  `;
+}
+
+// Edit User data
+function editUserData({
+  id,
+  name,
+  username,
+  email,
+  phone,
+  website,
+  address: { street, suite, city, zipcode },
+  company: { name: companyName, catchPhrase, bs },
+}) {
+  document.getElementById('userData').innerHTML = `
+    <div class="user-data">
+      <div>
+        <h6>PERSONAL DATA</h6>
+        <div>ID: ${id}</div>
+        <div><h4>Name: </h4><input type="text" placeholder="${name}" value="${name}" id="updateName"></div>
+        <div><h4>User Name: </h4><input type="text" placeholder="${username}" value="${username}" id="updateUserName"></div>
+        <div><h4>Email: </h4><input type="text" placeholder="${email}" value="${email}" id="updateEmail"></div>
+        <div><h4>Phone number: </h4><input type="text" placeholder="${phone}" value="${phone}" id="updatePhone"></div>
+        <div><h4>Website: </h4><input type="text" placeholder="${website}" value="${website}" id="updateWebsite"></div>
+      </div>
+      <div>
+        <h6>ADDRESS</h6>
+        <div><h4>Street: </h4><input type="text" placeholder="${street}" value="${street}" id="updateStreet"></div>
+        <div><h4>Suite: </h4><input type="text" placeholder="${suite}" value="${suite}" id="updateSuite"></div>
+        <div><h4>City: </h4><input type="text" placeholder="${city}" value="${city}" id="updateCity"></div>
+        <div><h4>Zipcode: </h4><input type="text" placeholder="${zipcode}" value="${zipcode}" id="updateZipcode"></div>
+      </div>
+      <div>
+        <h6>COMPANY</h6>
+        <div><h4>Company Name: </h4><input type="text" placeholder="${companyName}" value="${companyName}" id="updateCompanyName"></div>
+        <div><h4>Catch Phrase: </h4><input type="text" placeholder="${catchPhrase}" value="${catchPhrase}" id="updateCatchPhrase"></div>
+        <div><h4>BS: </h4><input type="text" placeholder="${bs}" value="${bs}" id="updateBs"></div>
+        <div><button id="submitEdit" type="submit">Save Changes</button></div>
+      </div>
+    </div>
+  `;
+  document.getElementById('submitEdit').addEventListener('click', function () {
+    const userEdit = {
+      name: document.getElementById('updateName').value,
+      username: document.getElementById('updateUserName').value,
+      email: document.getElementById('updateEmail').value,
+      phone: document.getElementById('updatePhone').value,
+      website: document.getElementById('updateWebsite').value,
+      address: {
+        street: document.getElementById('updateStreet').value,
+        suite: document.getElementById('updateSuite').value,
+        city: document.getElementById('updateCity').value,
+        zipcode: document.getElementById('updateZipcode').value,
+      },
+      company: {
+        name: document.getElementById('updateCompanyName').value,
+        catchPhrase: document.getElementById('updateCatchPhrase').value,
+        bs: document.getElementById('updateBs').value,
+      },
+    };
+    patchUser(id, userEdit).then((res) => {
+      const updatedData = data.map((item) => (item.id === res.id ? res : item));
+      renderTable(updatedData);
+      console.log(updatedData);
+    });
+    document.getElementById('popup').style.display = 'none';
+    // document.getElementById('userData').innerHTML = '';
+  });
+}
+
+document.getElementById('addNewUser').addEventListener('click', function () {
+  document.getElementById('popup').style.display = 'flex';
+  createNewUser();
+});
+
+// Delete User
+
+// Create new user
+function createNewUser() {
+  document.getElementById('userData').innerHTML = `
+    <div class="user-data">
+      <div>
+        <h6>PERSONAL DATA</h6>
+        <div><h4>Name: </h4><input type="text" placeholder="Name" value="Name" id="updateName"></div>
+        <div><h4>User Name: </h4><input type="text" placeholder="User name" value="User name" id="updateUserName"></div>
+        <div><h4>Email: </h4><input type="text" placeholder="Email" value="Email" id="updateEmail"></div>
+        <div><h4>Phone number: </h4><input type="text" placeholder="Phone number" value="Phone number" id="updatePhone"></div>
+        <div><h4>Website: </h4><input type="text" placeholder="Website" value="Website" id="updateWebsite"></div>
+      </div>
+      <div>
+        <h6>ADDRESS</h6>
+        <div><h4>Street: </h4><input type="text" placeholder="Street" value="Street" id="updateStreet"></div>
+        <div><h4>Suite: </h4><input type="text" placeholder="Suite" value="Suite" id="updateSuite"></div>
+        <div><h4>City: </h4><input type="text" placeholder="City" value="City" id="updateCity"></div>
+        <div><h4>Zipcode: </h4><input type="text" placeholder="Zipcode" value="Zipcode" id="updateZipcode"></div>
+      </div>
+      <div>
+        <h6>COMPANY</h6>
+        <div><h4>Company Name: </h4><input type="text" placeholder="Compamny name" value="Compamny name" id="updateCompanyName"></div>
+        <div><h4>Catch Phrase: </h4><input type="text" placeholder="Catch phrase" value="Catch phrase" id="updateCatchPhrase"></div>
+        <div><h4>BS: </h4><input type="text" placeholder="BS" value="BS" id="updateBs"></div>
+        <div><button id="submitEdit" type="submit">Save Changes</button></div>
+      </div>
+    </div>
+  `;
+  document.getElementById('submitEdit').addEventListener('click', function () {
+    const userEdit = {
+      name: document.getElementById('updateName').value,
+      username: document.getElementById('updateUserName').value,
+      email: document.getElementById('updateEmail').value,
+      phone: document.getElementById('updatePhone').value,
+      website: document.getElementById('updateWebsite').value,
+      address: {
+        street: document.getElementById('updateStreet').value,
+        suite: document.getElementById('updateSuite').value,
+        city: document.getElementById('updateCity').value,
+        zipcode: document.getElementById('updateZipcode').value,
+      },
+      company: {
+        name: document.getElementById('updateCompanyName').value,
+        catchPhrase: document.getElementById('updateCatchPhrase').value,
+        bs: document.getElementById('updateBs').value,
+      },
+    };
+    createUser(userEdit).then((res) => {
+      console.log(userEdit);
+      getUsers().then((res) => {
+        data = res;
+        renderTable(res);
+      });
+    });
+    document.getElementById('popup').style.display = 'none';
+    document.getElementById('userData').innerHTML = '';
+  });
 }
